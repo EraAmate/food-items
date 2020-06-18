22a6d9531303aa39247e99d9dc85d0ee072b428a
root <root@DESKTOP-9FU4U47.localdomain>
2020-06-18 10:48:20 +0200

    Install dev dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: ['eslint:recommended', 'prettier', 'plugin:react/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['prettier', 'react'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': 'warn'
+  }
+};
