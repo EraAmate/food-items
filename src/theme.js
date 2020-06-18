5e163f16d7c240770ac18ebe54fcd8438c7efef8
root <root@DESKTOP-9FU4U47.localdomain>
2020-06-18 11:54:54 +0200

    Create theme and implement it

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    bgPrimary: '#F4F4F4',
+    bgSecondary: '#839EAA',
+    basic: '#FFFFFF',
+    textPrimary: '#171615',
+    btnVeggie: '#0C8A00',
+    btnFruits: '#F87100',
+    btnMeat: '#F10000'
+  }
+};
+export default theme;
