555781a731660560e87b3c8eacce048f68692983
EraAmate <60475379+EraAmate@users.noreply.github.com>
2020-06-21 19:39:31 +0200

    Display food items and filter them by category (#7)
    * Create a filter component
    
    * Implement filter in home page
    
    * Implement mockData and add filter items function
    
    * Display food items cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,52 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import Header from '../components/Header';
+import ItemCards from '../components/ItemCards';
+import { mockItems } from '../db';
+import Filter from '../components/Filter';
 
 const Main = styled.div`
-  height: 100vh;
+  height: 100%;
   width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding: 20px;
   background: ${(props) => props.theme.colors.bgSecondary};
 `;
 
 const Home = () => {
+  const [items, setItems] = React.useState(mockItems);
+  const allItems = mockItems;
+
+  function filterItems(event) {
+    const inputName = event.target.name;
+    const isChecked = event.target.checked;
+
+    if (isChecked === true) {
+      let filteredItems = items.filter(function (item) {
+        if (item.category === inputName) {
+          return item;
+        }
+      });
+      setItems(filteredItems);
+    } else {
+      setItems(allItems);
+    }
+  }
   return (
     <>
       <Header brandname="Brandname" />
-      <Main></Main>
+      <Main>
+        <Filter
+          text1="meat"
+          text2="fruits"
+          text3="veggie"
+          onClick={filterItems}
+        />
+
+        <ItemCards items={items} />
+      </Main>
     </>
   );
 };
