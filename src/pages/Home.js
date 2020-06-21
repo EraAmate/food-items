9983d3f0c2d54ae24a0fa3ba761eddf25e8b9210
root <root@DESKTOP-9FU4U47.localdomain>
2020-06-21 19:29:23 +0200

    Implement filter in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import Header from '../components/Header';
+import Filter from '../components/Filter';
 
 const Main = styled.div`
-  height: 100vh;
+  height: 100%;
   width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding: 20px;
   background: ${(props) => props.theme.colors.bgSecondary};
 `;
 
@@ -15,7 +17,9 @@ const Home = () => {
   return (
     <>
       <Header brandname="Brandname" />
-      <Main></Main>
+      <Main>
+        <Filter text1="meat" text2="fruits" text3="veggie" />
+      </Main>
     </>
   );
 };
