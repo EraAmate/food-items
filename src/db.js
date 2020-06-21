b4c0427bbc5c7d9b24bf979145d7ef600833a203
root <root@DESKTOP-9FU4U47.localdomain>
2020-06-21 19:18:20 +0200

    Add mock data for the food items

---
+++ src/db.js
@@ -0,0 +1,74 @@
+export const mockItems = [
+  {
+    name: 'apple',
+    src:
+      'https://static.sscontent.com/prodimg/products/124/v678643_prozis_crunchy-red-apple-slices-20-g_2.jpg',
+    category: 'fruits',
+    article:
+      'Lorem ipsum dolor sit amet, consectetur adipisici elit, …“ ist ein Blindtext, der nichts bedeuten soll, sondern als Platzhalter im Layout verwendet wird, um einen Eindruck vom fertigen Dokument zu erhalten',
+    info: 'More info of this item'
+  },
+  {
+    name: 'brokoli',
+    src:
+      'https://www.westsideseattle.com/sites/default/files/images/%5Bdomain-url%5D/%5Bnode-yyyy%5D/%5Bnode-mm%5D/900_broccoli.png',
+    category: 'veggie',
+    article:
+      'Lorem ipsum dolor sit amet, consectetur adipisici elit, …“ ist ein Blindtext, der nichts bedeuten soll, sondern als Platzhalter im Layout verwendet wird, um einen Eindruck vom fertigen Dokument zu erhalten',
+    info: 'More info of this item'
+  },
+  {
+    name: 'chicken',
+    src:
+      'https://www.onceuponachef.com/images/2020/05/best-grilled-chicken-760x1050.jpg',
+    category: 'meat',
+    article:
+      'Lorem ipsum dolor sit amet, consectetur adipisici elit, …“ ist ein Blindtext, der nichts bedeuten soll, sondern als Platzhalter im Layout verwendet wird, um einen Eindruck vom fertigen Dokument zu erhalten',
+    info: 'More info of this item'
+  },
+  {
+    name: 'salat',
+    src:
+      'https://greenstar.coop/wp-content/uploads/2019/05/Salad_Greens-300x300.jpg',
+    category: 'veggie',
+    article:
+      'Lorem ipsum dolor sit amet, consectetur adipisici elit, …“ ist ein Blindtext, der nichts bedeuten soll, sondern als Platzhalter im Layout verwendet wird, um einen Eindruck vom fertigen Dokument zu erhalten',
+    info: 'More info of this item'
+  },
+  {
+    name: 'melone',
+    src:
+      'https://www.lecker.de/assets/styles/563x422/public/lecker/warenkunde/hbv_20/melone~1.jpg?itok=SygNuwWP',
+    category: 'fruits',
+    article:
+      'Lorem ipsum dolor sit amet, consectetur adipisici elit, …“ ist ein Blindtext, der nichts bedeuten soll, sondern als Platzhalter im Layout verwendet wird, um einen Eindruck vom fertigen Dokument zu erhalten',
+    info: 'More info of this item'
+  },
+  {
+    name: 'pork',
+    src:
+      'https://www.rockrecipes.com/wp-content/uploads/2020/04/Brined-Roast-Pork-Loin-close-up-photo-of-sliced-pork.jpg',
+    category: 'meat',
+    article:
+      'Lorem ipsum dolor sit amet, consectetur adipisici elit, …“ ist ein Blindtext, der nichts bedeuten soll, sondern als Platzhalter im Layout verwendet wird, um einen Eindruck vom fertigen Dokument zu erhalten',
+    info: 'More info of this item'
+  },
+  {
+    name: 'orange',
+    src:
+      'https://images.unsplash.com/photo-1582979512210-99b6a53386f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=300',
+    category: 'fruits',
+    article:
+      'Lorem ipsum dolor sit amet, consectetur adipisici elit, …“ ist ein Blindtext, der nichts bedeuten soll, sondern als Platzhalter im Layout verwendet wird, um einen Eindruck vom fertigen Dokument zu erhalten',
+    info: 'More info of this item'
+  },
+  {
+    name: 'tomate',
+    src:
+      'https://volmary.de/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/g/e/gemuese-tomaten-geschmackstomate-sportivo-pflanzen-volmary-03.jpg',
+    category: 'veggie',
+    article:
+      'Lorem ipsum dolor sit amet, consectetur adipisici elit, …“ ist ein Blindtext, der nichts bedeuten soll, sondern als Platzhalter im Layout verwendet wird, um einen Eindruck vom fertigen Dokument zu erhalten',
+    info: 'More info of this item'
+  }
+];
