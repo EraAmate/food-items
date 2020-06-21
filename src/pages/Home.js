a967fd710118dfd494a5e9b5c8d5b9311dcb3a09
root <root@DESKTOP-9FU4U47.localdomain>
2020-06-21 19:31:50 +0200

    Implement mockData and add filter items function

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import Header from '../components/Header';
+import { mockItems } from '../db';
 import Filter from '../components/Filter';
 
 const Main = styled.div`
@@ -14,11 +15,34 @@ const Main = styled.div`
 `;
 
 const Home = () => {
+  const [items, setItems] = React.useState(mockItems);
+  const allItems = mockItems;
+
+  function filterItems(event) {
+    const inputName = event.target.name;
+    const isChecked = event.target.checked;
+
+    if (isChecked === true) {
+      let filteredItems = items.filter(function (item) {
+        if (item.category === inputName) {
+          return item;
+        }
+      });
+      setItems(filteredItems);
+    } else {
+      setItems(allItems);
+    }
+  }
   return (
     <>
       <Header brandname="Brandname" />
       <Main>
-        <Filter text1="meat" text2="fruits" text3="veggie" />
+        <Filter
+          text1="meat"
+          text2="fruits"
+          text3="veggie"
+          onClick={filterItems}
+        />
       </Main>
     </>
   );
