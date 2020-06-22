110894785078d206ead17faefab0236aac0c840e
root <root@DESKTOP-9FU4U47.localdomain>
2020-06-22 13:57:53 +0200

    Rename food items

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 export const mockItems = [
   {
-    name: 'apple',
+    name: 'Apple',
     src:
       'https://static.sscontent.com/prodimg/products/124/v678643_prozis_crunchy-red-apple-slices-20-g_2.jpg',
     category: 'fruits',
@@ -9,7 +9,7 @@ export const mockItems = [
     info: 'More info of this item'
   },
   {
-    name: 'brokoli',
+    name: 'Brokoli',
     src:
       'https://www.westsideseattle.com/sites/default/files/images/%5Bdomain-url%5D/%5Bnode-yyyy%5D/%5Bnode-mm%5D/900_broccoli.png',
     category: 'veggie',
@@ -18,7 +18,7 @@ export const mockItems = [
     info: 'More info of this item'
   },
   {
-    name: 'chicken',
+    name: 'Chicken',
     src:
       'https://www.onceuponachef.com/images/2020/05/best-grilled-chicken-760x1050.jpg',
     category: 'meat',
@@ -27,7 +27,7 @@ export const mockItems = [
     info: 'More info of this item'
   },
   {
-    name: 'salat',
+    name: 'Salat',
     src:
       'https://greenstar.coop/wp-content/uploads/2019/05/Salad_Greens-300x300.jpg',
     category: 'veggie',
@@ -36,7 +36,7 @@ export const mockItems = [
     info: 'More info of this item'
   },
   {
-    name: 'melone',
+    name: 'Melone',
     src:
       'https://www.lecker.de/assets/styles/563x422/public/lecker/warenkunde/hbv_20/melone~1.jpg?itok=SygNuwWP',
     category: 'fruits',
@@ -45,7 +45,7 @@ export const mockItems = [
     info: 'More info of this item'
   },
   {
-    name: 'pork',
+    name: 'Pork',
     src:
       'https://www.rockrecipes.com/wp-content/uploads/2020/04/Brined-Roast-Pork-Loin-close-up-photo-of-sliced-pork.jpg',
     category: 'meat',
@@ -54,7 +54,7 @@ export const mockItems = [
     info: 'More info of this item'
   },
   {
-    name: 'orange',
+    name: 'Orange',
     src:
       'https://images.unsplash.com/photo-1582979512210-99b6a53386f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=300',
     category: 'fruits',
@@ -63,7 +63,7 @@ export const mockItems = [
     info: 'More info of this item'
   },
   {
-    name: 'tomate',
+    name: 'Tomate',
     src:
       'https://volmary.de/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/g/e/gemuese-tomaten-geschmackstomate-sportivo-pflanzen-volmary-03.jpg',
     category: 'veggie',
