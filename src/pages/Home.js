29d7f2fdf0fe8a0262701aef6b56469c7bf7b370
root <root@DESKTOP-9FU4U47.localdomain>
2020-06-22 14:08:36 +0200

    Improve filter function

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,39 +11,53 @@ const Main = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 20px;
+  padding: 10px 40px 20px 40px;
   background: ${(props) => props.theme.colors.bgSecondary};
 `;
 
 const Home = () => {
   const [items, setItems] = React.useState(mockItems);
+  const [filters, setFilters] = React.useState([
+    { isChecked: true, name: 'meat', color: 'red' },
+    { isChecked: true, name: 'fruits', color: 'orange' },
+    { isChecked: true, name: 'veggie', color: 'green' }
+  ]);
   const allItems = mockItems;
 
   function filterItems(event) {
-    const inputName = event.target.name;
-    const isChecked = event.target.checked;
+    const checkboxName = event.target.name;
+    const newFilters = filters.map((filter) => {
+      if (filter.name === checkboxName) {
+        filter.isChecked = !filter.isChecked;
+      }
+      return filter;
+    });
 
-    if (isChecked === true) {
-      let filteredItems = items.filter(function (item) {
-        if (item.category === inputName) {
-          return item;
-        }
-      });
-      setItems(filteredItems);
-    } else {
-      setItems(allItems);
-    }
+    setFilters(newFilters);
+
+    const activeFilters = filters.map((filter) => {
+      if (filter.isChecked) return filter.name;
+    });
+
+    const filteredItems = activeFilters.reduce((acc, value) => {
+      const filteredItems = allItems.filter(filterBy(value));
+      // Possible Improvment, sort so that the last clicked filtered items show up first
+      //filteredItems.sort((a, b) => a.category.localeCompare(b.category));
+      return [...acc, ...filteredItems];
+    }, []);
+
+    setItems(filteredItems);
   }
+
+  const filterBy = (category) => (item) => {
+    return item.category === category;
+  };
+
   return (
     <>
       <Header brandname="Brandname" />
       <Main>
-        <Filter
-          text1="meat"
-          text2="fruits"
-          text3="veggie"
-          onClick={filterItems}
-        />
+        <Filter filters={filters} onChange={filterItems} />
 
         <ItemCards items={items} />
       </Main>
